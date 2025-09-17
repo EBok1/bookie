1a4c186e593afad8cd43154de7e1e1bfb73a06bd
EBok1 <ellenoorbok@icloud.com>
2025-09-17 16:01:20 +0200

    Added logo img

---
+++ src/screens/landing/Logo.jsx
@@ -0,0 +1,13 @@
+function Logo() {
+  return (
+    <>
+      <img
+        src="/logo.png"
+        alt="Logo"
+        className="h-20 fixed justify-items-start ml-2"
+      />
+    </>
+  );
+}
+
+export default Logo;
