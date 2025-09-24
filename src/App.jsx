bc9ee1a2fcf0479d1f80955f2126e664766f8c08
EBok1 <ellenoorbok@icloud.com>
2025-09-24 14:21:27 +0200

    Deleted functions and made components of them

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,20 @@
-import Nav from "./screens/landing/Navbar.jsx";
-import Books from "./screens/landing/Books.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function Home() {
-  return (
-    <>
-      <h1 className="text-center text-5xl leading-tight font-serif">
-        Welcome to Bookie
-      </h1>
-      <div className="text-center max-w-2xl mx-auto p-8">
-        <p className="text-xl mb-4 font-semibold text-green-600">
-          Your personal reading companion awaits!
-        </p>
-        <p className="text-base">
-          Rate your favorites, discover new adventures, and keep track of all
-          those books you swear you'll get to... someday. Because let's be
-          honest, we all have that one shelf that's practically bursting!
-        </p>
-        <p className="text-lg mb-6 italic text-gray-700">
-          "A room without books is like a body without a soul" - Cicero
-        </p>
-        <img
-          src="/front-page.png"
-          alt="Image of drawn books"
-          className="h-auto w-screen"
-        />
-      </div>
-      <Books />
-    </>
-  );
-}
-
-function About() {
-  return (
-    <div className="p-8">
-      <h1 className="text-3xl">About Page</h1>
-      <p>About content here...</p>
-    </div>
-  );
-}
-
-function Contact() {
-  return(
-    <div>
-      <h1>Contact page</h1>
-    </div>
-  )
-}
-
-function Login() {
-  return (
-    <div>
-      <h1>Login page</h1>
-    </div>
-  )
-}
+import BookDetailPage from "./pages/BookDetailPage/BookDetailPage.jsx";
+import BookOverviewPage from "./pages/BookOverviewPage/BookOverviewPage.jsx";
+import Navigation from "./components/Navigation/Navigation.jsx";
+import FavoritesList from "./pages/FavoriteListPage/FavoriteListPage.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <Nav />
+      <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<BookOverviewPage />} />
+        {/* <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} /> */}
+        <Route path="/favorites" element={<FavoritesList />} />
+        <Route path="/book/:id" element={<BookDetailPage />} />
       </Routes>
     </BrowserRouter>
   );
