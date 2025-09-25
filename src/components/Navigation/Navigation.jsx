d21522dd0fcfda7d8b72cfc62bf2b2e4a6ada1a7
EBok1 <ellenoorbok@icloud.com>
2025-09-25 10:41:23 +0200

    Added sticky menu and not sticky logo

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Logo from "../Logo/Logo";
 import { Link } from "react-router-dom";
 
@@ -9,14 +9,13 @@ function Dropdown() {
   };
   return (
     <div className="relative flex justify-between mr-6 mb-4 ml-3">
-      <Logo />
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
       ></link>
       <button
         onClick={toggleDropdown}
-        className="text-lg font-semibold font-quicksand"
+        className="text-lg font-semibold"
       >
         Menu <i className="fa fa-bars pl-1"></i>
       </button>
@@ -44,13 +43,39 @@ function Dropdown() {
 }
 
 function Navigation() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true); // User has scrolled down
+      } else {
+        setIsScrolled(false); // User is at top
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <>
-      <div className="sticky top-0">
-        <div className="sm:hidden">
+      {!isScrolled ? (
+        // Initial layout: Logo and menu on same line
+        <div className="sm:hidden flex justify-between items-center mr-3 mb-4 ml-3">
+          <Logo />
           <Dropdown />
         </div>
-      </div>
+      ) : (
+        // Scrolled layout: Only sticky menu
+        <>
+          <div className="sm:hidden mr-6 mb-4 ml-3">
+            <Logo />
+          </div>
+          <div className="sm:hidden fixed top-0 right-3 z-10 py-2">
+            <Dropdown />
+          </div>
+        </>
+      )}
       {/* Desktop Version - Hidden on mobile */}
 
       <nav className="hidden sm:flex justify-between items-center mr-6 mb-4 ml-3">
