f047dbe8f6584c2a99bd97b189dd419443bb6799
EBok1 <ellenoorbok@icloud.com>
2025-10-06 11:36:36 +0200

    Changed styling

--- src/components/BookCardGrid/BookCardGrid.jsx
+++ src/components/BookCardGrid/BookCardGrid.jsx
@@ -1,7 +1,7 @@
 function BookCardGrid({ children }) {
   return (
     <>
-     <div className="max-w-4xl mx-auto p-6">
+     <div className="max-w-4xl mx-auto px-6">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {children}
       </div>
