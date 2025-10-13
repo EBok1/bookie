1e86f8ec2573f4d9bab77090814c0aa98aa3a42e
EBok1 <ellenoorbok@icloud.com>
2025-10-13 14:11:32 +0200

    Made booktag component with props

---
+++ src/components/BookTag/BookTag.jsx
@@ -0,0 +1,18 @@
+const BookTag = ({ tag, variant, hashtag, ...props }) => {
+  const variants = {
+    orange:
+      "inline-block bg-orange-100 text-orange-800 px-3 py-1 mb-8 rounded-full text-sm font-medium",
+    grey: "bg-gray-100 text-gray-700 px-2 py-1 rounded text-sm",
+    green: "bg-[#bccdbc] px-3 py-1 rounded-full text-sm font-medium",
+  };
+  return (
+    <>
+      <span className={variants[variant]} {...props}>
+        {hashtag ? "#" : ""}
+        {tag}
+      </span>
+    </>
+  );
+};
+
+export default BookTag;
