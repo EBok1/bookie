e932f74b8a14a6b457cc113615f7c7ca6791aa3b
EBok1 <ellenoorbok@icloud.com>
2025-10-13 14:12:01 +0200

    Made bookdetails component

---
+++ src/components/BookDetails/BookDetails.jsx
@@ -0,0 +1,35 @@
+const BookDetails = ({bookData}) => {
+    return(
+        <>
+        <h3 className="text-xl font-bold text-gray-800 mb-2 font-playfair">
+            Book Details
+          </h3>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <dt className="text-sm font-medium text-gray-500">
+                Published Year
+              </dt>
+              <dd className="text-gray-900">{bookData.publishedYear}</dd>
+            </div>
+            <div>
+              <dt className="text-sm font-medium text-gray-500">Pages</dt>
+              <dd className="text-gray-900">{bookData.pages}</dd>
+            </div>
+            <div>
+              <dt className="text-sm font-medium text-gray-500">Publisher</dt>
+              <dd className="text-gray-900">{bookData.publisher}</dd>
+            </div>
+            <div>
+              <dt className="text-sm font-medium text-gray-500">Language</dt>
+              <dd className="text-gray-900">{bookData.language}</dd>
+            </div>
+            <div>
+              <dt className="text-sm font-medium text-gray-500">Format</dt>
+              <dd className="text-gray-900">{bookData.format}</dd>
+            </div>
+          </div>
+        </>
+    )
+}
+
+export default BookDetails;
