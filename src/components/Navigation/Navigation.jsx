962d26a3581b205ded8e5c0f5688a901ec5c9d1a
EBok1 <ellenoorbok@icloud.com>
2025-10-13 10:05:02 +0200

    Added scroll function and changed styling

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,44 +2,70 @@ import { useState, useEffect } from "react";
 import Logo from "../Logo/Logo";
 import { Link } from "react-router-dom";
 
-function Dropdown() {
+function Dropdown({ onToggle }) {
   const [isOpen, setIsOpen] = useState(false);
+
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
+    onToggle(!isOpen);
   };
 
   const closeDropdown = () => {
     setIsOpen(false);
-  }
+    onToggle(false);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (isOpen) {
+        setIsOpen(false);
+        onToggle(false);
+      }
+    };
+    if (isOpen) {
+      window.addEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [isOpen, onToggle]);
 
   return (
-    <div className="relative flex justify-between mr-6 mb-4 ml-3">
+    <div className="relative flex justify-between mr-6 ml-3">
       <link
         rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
       ></link>
-      <button
-        onClick={toggleDropdown}
-        className="text-lg font-semibold"
-      >
+      <button onClick={toggleDropdown} className="text-lg font-semibold">
         Menu <i className="fa fa-bars pl-1"></i>
       </button>
       {isOpen && (
-        <ul className="fixed top-14 left-0 w-screen bg-white border shadow flex flex-col items-center py-4 gap-6">
-          <li>
-            <Link to="/" onClick={closeDropdown}>Home</Link>
+        <ul className="fixed top-[6.25rem] left-5 right-5 bg-[#fffdf6] shadow-md flex flex-col items-center py-4 gap-4 rounded-b border border-[#A55A16]">
+          <li className="w-full justify-center items-center flex border-b border-[#A55A16] pb-4">
+            <Link to="/" onClick={closeDropdown}>
+              Home
+            </Link>
+          </li>
+          <li className="w-full justify-center items-center flex border-b border-[#A55A16] pb-4">
+            <Link to="/about" onClick={closeDropdown}>
+              About
+            </Link>
+          </li>
+          <li className="w-full justify-center items-center flex border-b border-[#A55A16] pb-4">
+            <Link to="/contact" onClick={closeDropdown}>
+              Contact
+            </Link>
+          </li>
+          <li className="w-full justify-center items-center flex border-b border-[#A55A16] pb-4">
+            <Link to="/login" onClick={closeDropdown}>
+              Login
+            </Link>
           </li>
           <li>
-            <Link to="/about" onClick={closeDropdown}>About</Link>
-          </li>
-          <li>
-            <Link to="/contact" onClick={closeDropdown}>Contact</Link>
-          </li>
-          <li>
-            <Link to="/login" onClick={closeDropdown}>Login</Link>
-          </li>
-          <li>
-            <Link to="/favorites" onClick={closeDropdown}>Favorites❤️</Link>
+            <Link to="/favorites" onClick={closeDropdown}>
+              Favorites
+            </Link>
           </li>
         </ul>
       )}
@@ -48,40 +74,18 @@ function Dropdown() {
 }
 
 function Navigation() {
-  const [isScrolled, setIsScrolled] = useState(false);
+  const [showBorder, setShowBorder] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsScrolled(true); // User has scrolled down
-      } else {
-        setIsScrolled(false); // User is at top
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
   return (
     <>
-      {!isScrolled ? (
-        // Initial layout: Logo and menu on same line
-        <div className="sm:hidden flex justify-between items-center mr-3 mb-4 ml-3">
-          <Logo />
-          <Dropdown />
-        </div>
-      ) : (
-        // Scrolled layout: Only sticky menu
-        <>
-          <div className="sm:hidden mr-6 mb-4 ml-3">
-            <Logo />
-          </div>
-          <div className="sm:hidden fixed top-0 right-3 z-10 py-2">
-            <Dropdown />
-          </div>
-        </>
-      )}
-      {/* Desktop Version - Hidden on mobile */}
+      <div
+        className={`sm:hidden flex justify-between items-center bg-[#fffdf6] rounded-md shadow-md mx-5 mt-5 ${
+          showBorder ? "border border-[#A55A16] rounded-b-none" : ""
+        }`}
+      >
+        <Logo />
+        <Dropdown onToggle={setShowBorder} />
+      </div>
 
       <nav className="hidden sm:flex justify-between items-center mr-6 mb-4 ml-3">
         <div>
@@ -105,7 +109,7 @@ function Navigation() {
             Login
           </Link>
           <Link className="hover:text-blue-500" to="/favorites">
-            Favorites❤️
+            Favorites
           </Link>
         </div>
       </nav>
