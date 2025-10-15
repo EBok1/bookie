ab129db35060a3cbb1ec20b15dcef9817c56cbb2
EBok1 <ellenoorbok@icloud.com>
2025-10-15 14:31:58 +0200

    Refactor: remove unused files and migrate to server components
    - Remove app/book/[id]/BookDetailPage.jsx (replaced by page.jsx server component)
    - Remove app/mocks/helpers.js (functionality moved to server hooks)
    - Remove components/BookReview/utils/getBookCommentsById.js (replaced by server-side fetch)
    
    These files are no longer needed due to the migration from client-side
    data fetching to Next.js server components architecture. The functionality
    has been moved to more appropriate locations following Next.js 13+ patterns.

--- app/mocks/helpers.js
+++
@@ -1,8 +0,0 @@
-import booksList from "./books.json";
-
-const fetchBookById = ({id}) => {
-    const foundBook = booksList.find((book) => book.id == id);
-    return foundBook; 
-}
-
-export default fetchBookById;
