66b04d3dd83903778c083cb9cd33e53e6449bff1
EBok1 <ellenoorbok@icloud.com>
2025-10-22 09:51:27 +0200

    Changed styling

--- components/BookCard/BookCard.jsx
+++ components/BookCard/BookCard.jsx
@@ -13,14 +13,14 @@ function BookCard({ book }) {
         className="border rounded-lg p-4 shadow-md bg-[#fffdf6]"
       >
         <FallbackImage book={book} />
-        <h3 className="font-bold text-xl mb-4 justify-center font-playfair">
+        <h3 className="font-bold text-xl justify-center font-playfair">
           {book.title}
         </h3>
-        <p className="text-gray-600 mt-4 mb-2">{book.author}</p>
+        <p className="text-gray-600 mb-2">{book.author}</p>
         <AverageBookRating averageRating={book.calculatedAverage}/>
-        <div className="flex justify-between flex-wrap gap-4">
+        <div className="flex justify-between flex-wrap gap-4 mt-4">
           <p>
-            <BookTag tag={book.genre} variant="green" />
+            <BookTag tag={book.genre} variant="orange" />
           </p>
           <LikeButton book={book} />
         </div>
